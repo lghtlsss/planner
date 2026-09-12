5b5c31261461a21d8666767869b30dbb76305c29
artem <guryanovartem2008@gmail.com>
2026-09-12 22:32:35 +0300

    get user by id method

--- app/routers/users.py
+++ app/routers/users.py
@@ -16,16 +16,16 @@ def get_users(db: Session = Depends(get_db)):
     return db.execute(select(User)).scalars().all()
 
 
+@router.get("{user_id}")
+def get_single_user(user_id: int, db: Session = Depends(get_db)):
+    return db.get(User, user_id)
+
+
 @router.post("", response_model=SUserResponse)
 def create_user(user: SUserCreate, db: Session = Depends(get_db)):
     db_user = User(name=user.name,
                    surname=user.surname,
                    hashed_password=user.password)
-    # del user["password"]
-    # data = user.model_dump()
-    #
-    # for key, value in data.items():
-    #     setattr(db_user, key, value)
 
     db.add(db_user)
     db.commit()
