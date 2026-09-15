e590de42ab6c80bae2fb81d8091994252e452fa6
1lght <guryanovartem2008@gmail.com>
2026-09-15 17:58:55 +0300

    апи ключ гугл таблиц добавил в конфиг

--- backend/app/config.py
+++ backend/app/config.py
@@ -6,6 +6,7 @@ class Settings(BaseSettings):
     database_url: str
     jwt_access_token_expire_minutes: int
     jwt_algorithm: str
+    google_sheets_api_key: str
 
     model_config = SettingsConfigDict(env_file=".env")
 
